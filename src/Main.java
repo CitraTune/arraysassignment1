c007f9ea168f5b9c646e0f3fb4b16d75381a4aaf
jkeating2025 <jkeating2025@cchsdons.com>
2024-02-09 12:52:50 -0800

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[] weekdays = new String[] {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        System.out.println("All the days of the week:");
+        for(int i = 0; i < weekdays.length; i++){
+            System.out.println(weekdays[i]);
+        }
+        System.out.println(" \nBusiness days:");
+        weekdays = new String[]{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        for(int i = 0; i < weekdays.length; i++){
+            System.out.println(weekdays[i]);
+        }
+        System.out.println(" \nBusiness days shuffled");
+        int[] randomizer = new int[weekdays.length];
+        int[] count = {1,2,3,4,5};
+        //problem lies in assigning new values to it every single time. need to prevent repeats. how to..? maybe make the array already full of numbers and take away them after setting one to it
+        for(int i = 0; i < randomizer.length; i++){
+            int swap = (int)(Math.random() * randomizer.length);
+            //goal: prevent repeats in count
+            randomizer[i] = (int)(Math.random() * randomizer.length);
+        }
+        for(int i = 0; i < randomizer.length; i++){
+            System.out.println(randomizer[i]);
+        }
+
+
+//        for(int i = 0; i < weekdays.length; i++){
+//            int swap = (int)(Math.random() * weekdays.length);
+//            while(i == swap) {
+//                swap = (int)(Math.random() * weekdays.length);
+//            }
+//            System.out.println("Swapped " + weekdays[swap] + " and " + weekdays[i]);
+//            weekdays[i] = weekdays[swap];
+//            weekdays[swap] = weekdays[i];
+//
+//        }
+        for(int i = 0; i < weekdays.length; i++){
+            System.out.println(weekdays[i]);
+        }
+
     }
 }
