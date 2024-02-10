770292d43c0a7c8f4a729eea35d05f6d84748586
jkeating2025 <jkeating2025@cchsdons.com>
2024-02-09 17:10:29 -0800

    a

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,25 @@ public class Main {
 
         String[] weekdaysTemp = weekdays;
         //problem lies in assigning new values to it every single time. need to prevent repeats. how to..? maybe make the array already full of numbers and take away them after setting one to it
-        for(int i = 0; i < weekdays.length; i++){
-            //randomNum is a number
+        for(int i = 0; i < weekdaysTemp.length; i++){
+            //randomNum is a number that is within weekdaysTemp's length
             int randomNum = (int)(Math.random()* weekdaysTemp.length);
+            //randomDay is picked from weekdaysTemp
             String randomDay = weekdaysTemp[randomNum];
+            //if randomDay exists skip this step
             while (randomDay == null){
+                //if randomDay is null and doesnt exist then follow this step
+                //randomNum is repicked from the same pool
                 randomNum = (int)(Math.random()* weekdaysTemp.length);
+                //Therefore repicking randomday
                 randomDay = weekdaysTemp[randomNum];
-                System.out.println("got null");
+                //Reports what number is null
+                System.out.println("got null at "+ randomNum);
+                //loops and compares randomDay again
             }
-            //remove randomDay from the temporary weekdays cache
+            //removes location of randomDay from the temporary weekdays cache if we make it to this point, whiuch should mean that weekdaysTemp at this number does exist.
             weekdaysTemp[randomNum] = null;
+            System.out.println("Set "+ randomDay+ + randomNum+ " in weekdaysTemp to null");
             //set the weekday of the current iteration to the randomly selected day
             weekdays[i] = randomDay;
         }
